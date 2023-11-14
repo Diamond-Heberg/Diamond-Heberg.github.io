c2e8176b2f1d520a0823994b73214c16c2a21331
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2023-11-14 21:28:41 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+      themes: [
+        {
+          diamondblack: {
+          
+ "primary": "#93c5fd",
+          
+ "secondary": "#c084fc",
+          
+ "accent": "#60a5fa",
+          
+ "neutral": "#1f2937",
+          
+ "base-100": "#0c4a6e",
+          
+ "info": "#c4b5fd",
+          
+ "success": "#68fd97",
+          
+ "warning": "#ffa100",
+          
+ "error": "#ff7573",
+          },
+        },
+      ],
+    },
+  plugins: [require("daisyui")],
+}
+
